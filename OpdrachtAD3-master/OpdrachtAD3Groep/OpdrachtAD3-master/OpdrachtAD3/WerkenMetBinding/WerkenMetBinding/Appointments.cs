0bb37927644a76fc9687a79426381db73fe584a3
JefGeeraerts <32387101+JefGeeraerts@users.noreply.github.com>
2018-12-28 23:04:19 +0100

    alles samen

---
+++ OpdrachtAD3-master/OpdrachtAD3Groep/OpdrachtAD3-master/OpdrachtAD3/WerkenMetBinding/WerkenMetBinding/Appointments.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ad3OpdrachtSamGhesquiere
+{
+    public class Appointments : ObservableCollection<Appointment>
+    {
+        public Appointments()
+        {
+            CollectionChanged += Appointments_CollectionChanged;
+        }
+
+        private void Appointments_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+                foreach (IAppointment Appointment in e.NewItems)
+                    if (Appointment != null)
+                        Appointment.PropertyChanged += Appointment_PropertyChanged;
+            if (e.OldItems != null)
+                foreach (IAppointment Appointment in e.OldItems)
+                    if (Appointment != null)
+                        Appointment.PropertyChanged -= Appointment_PropertyChanged;
+        }
+
+        private void Appointment_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnCollectionChanged(new System.Collections.Specialized.NotifyCollectionChangedEventArgs(System.Collections.Specialized.NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
---
+++ OpdrachtAD3-master/OpdrachtAD3Groep/OpdrachtAD3-master/OpdrachtAD3/WerkenMetBinding/WerkenMetBinding/IAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ad3OpdrachtSamGhesquiere
+{
+    public interface IAppointment : INotifyPropertyChanged
+    {   
+        String Name { get; set; }
+        DateTime DateStart { get; set; }
+        DateTime DateEnd { get; set; }
+        String Description { get; set; }
+        Boolean PriorityLevel { get; set; }
+    }
+}
+
